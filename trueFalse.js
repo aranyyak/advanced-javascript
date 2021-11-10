79e1e225ae00cba84648d17789db4138e598f204
aranyyak <aranyak.amin@gmail.com>
2021-11-10 19:25:30 +0600

    truefalse

---
+++ trueFalse.js
@@ -0,0 +1,20 @@
+// 0, "", undefined, null, NaN => false value;
+// Number, '0', " ", [] => true value;
+
+// const age = 0;
+
+// if(age){
+//     console.log("condition is true");
+// }
+// else{
+//     console.log("condition is false");
+// }
+
+const name = "samsu";
+
+if(name){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
