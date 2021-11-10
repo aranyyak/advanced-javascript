af63e4b028ce02ecee38c0e046bc7609dd9119f5
aranyyak <aranyak.amin@gmail.com>
2021-11-10 19:48:59 +0600

    null-vs-undefind

---
+++ null-vs-undefined.js
@@ -0,0 +1,42 @@
+// let pakhi;
+// console.log(pakhi); 
+// if no value set, it will show undefined
+
+function add(num1, num2) {
+    num1+num2;
+    // if no return set, it will show undefined
+}
+
+const result = add(12, 54);
+// console.log(result);
+
+
+
+function add(num1, num2) {
+    num1+num2;
+    // if any parameter of function is not passed, it will show undefined
+}
+
+const result1 = add(12);
+// console.log(result1);
+
+
+
+const premik = {name:"kopa samsu", phn:4525555};
+// console.log(premik.gf);
+// if any property is not present in object, it will show undefined
+
+
+
+
+
+
+
+let fun = undefined;
+// console.log(fun); 
+// it will show undefined
+
+
+
+null
+// null is empty or not existend of a element
