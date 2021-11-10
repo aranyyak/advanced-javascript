8ca075e229dbaaa729d1a377765106cecd462002
aranyyak <aranyak.amin@gmail.com>
2021-11-10 19:59:56 +0600

    double-vs-tripple-equal

---
+++ double-vs-tripple-equal.js
@@ -0,0 +1,18 @@
+const first = 0;
+const second = true;
+
+if(first == second){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
+
+// Double equal will check the value or convert the value an campare. tripple equal will compare the value strictly
+
+if(first === second){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
