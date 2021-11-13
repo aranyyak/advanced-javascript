f8a0f92d98d7b91d1ddc8096fcefb8e3be873475
aranyyak <aranyak.amin@gmail.com>
2021-11-13 14:26:53 +0600

    done all

---
+++ arguments.js
@@ -0,0 +1,7 @@
+function add(num1, num2) {
+    const numbers = [...arguments];
+    
+}   
+
+const result = add(2, 3, 5);
+console.log(result);
---
+++ array-split.js
@@ -0,0 +1,9 @@
+const nums = [2, 3, 4, 5, 6, 7, 8];
+// const part = nums.slice(2, 5);
+
+// const removed = nums.splice(2, 4);
+
+const add = nums.join("+")
+
+console.log(add);
+console.log(nums);
